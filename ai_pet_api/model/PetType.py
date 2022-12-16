3cbb1ca0ac4cbfb2a612a9b48f92a52611b3474f
Jisoo Han <jisooh@wepay.com>
2022-12-15 21:52:19 -0800

    Add post API to create pet and add some basic models for pet and type enums

---
+++ ai_pet_api/model/PetType.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class PetType(str, Enum):
+    DOG = 'dog'
+    CAT = 'cat'
+    RABBIT = 'rabbit'
+    FISH = 'fish'
+    ROCK = 'rock'
+    PLANT = 'plant'
+    HORSE = 'horse'
+    COW = 'cow'
+    SHEEP = 'sheep'
+    PENGUIN = 'penguin'
+    SHARK = 'shark'
+    JELLYFISH = 'jellyfish'
+    STEAK = 'steak'
+    BREAD = 'bread'
+    GHOST = 'ghost'
